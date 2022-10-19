6d65d23023b0d3654977a905b8eb9ca63218ee59
Pogiwogi <116036952+Pogiwogi@users.noreply.github.com>
2022-10-19 14:44:58 -0700

    Update FootControlWirelessTransmitter.ino
    Deleted Testservormotor

--- Foot Control/FootControlWirelessTransmitter/FootControlWirelessTransmitter.ino
+++ Foot Control/FootControlWirelessTransmitter/FootControlWirelessTransmitter.ino
@@ -12,7 +12,7 @@
 RF24 radio(7, 8); // CE, CSN refers to the nRF transmitter    
 uint8_t address[][6] = { "1Node", "2Node" };
 bool radioNumber = 0; // 
-int payload[3] = {0,0,1}; // [Type, Index, Value] refer to readme: https://github.com/RavingPlatypi/Smart-Prosthetics-2022-2023/blob/main/Foot%20Control/README.md
+int payload[3] = {0,0,0}; // [Type, Index, Value] refer to readme: https://github.com/RavingPlatypi/Smart-Prosthetics-2022-2023/blob/main/Foot%20Control/README.md
 
 int btnPins[] = {2}; // Joystick button 
 const int numBtnPins = (sizeof(btnPins) / sizeof(btnPins[0])); // Purpose to count the number of buttons, incase we want to add more
@@ -33,7 +33,7 @@ void setup() {
     Serial.println("Radio hardware is not responding!");
     while (1) {}  // hold in infinite loop
   }
-  // Need every radio.(method) in order for nRF to work eve
+  // Need every radio.(method) in order for nRF to work
   radio.setPALevel(RF24_PA_LOW);
   radio.openWritingPipe(address[radioNumber]);
   radio.stopListening(); // This method is not used, but still must be included
@@ -43,12 +43,12 @@ void setup() {
 }
 
 void loop() {
-  // Read button values and transmit.
+  // Read button values and transmit
   int btnState = digitalRead(btnPins[0]);
   transmitPayload(0,0,!btnState);
   
 
-  // Read potentiometer values and transmit.
+  // Read potentiometer values and transmit
   for (int i = 0; i < numPotPins; i++){
     potValues[i] = analogRead(potPins[i]);
     int potValue = map(potValues[i], 512, 1024, 0, 120);
@@ -72,15 +72,4 @@ bool transmitPayload(int type, int num, int value){
   Serial.println(
     sent ? "transmission sent" : "failed to send transmission"
   );
-}
-
-/*
-Below is the function for testing a Servomotor.
-Put this in the loop to make a Servomotor rotate 180 deg and back.
-*/
-void testServoMessage() {
-  int payloadBtn[3] = {1,0,testMotorPos};
-  bool sent = radio.write(&payloadBtn, sizeof(payload));
-  testMotorPos++;
-  if (testMotorPos >= 180) testMotorPos = 0;
 }
