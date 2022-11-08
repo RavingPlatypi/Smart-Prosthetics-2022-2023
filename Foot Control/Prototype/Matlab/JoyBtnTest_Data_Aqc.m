27a56bff48f3dba178268bd7cf8a7a4404595f51
Pogiwogi <116036952+Pogiwogi@users.noreply.github.com>
2022-11-07 23:40:03 -0800

    Adding Joy, JoyBtn, Btn

--- Foot Control/Prototype/Matlab/JoyBtnTest_Data_Aqc.m
+++ Foot Control/Prototype/Matlab/JoyBtnTest_Data_Aqc.m
@@ -4,17 +4,23 @@ a = arduino;
 
 joy = zeros(500,1);
 joyBtn = zeros(500,1);
+btn = zeros(500,1);
 t = seconds(joy);
-%%
+
 
 t0 = datetime('now');
 for ii = 1:500
     joy(ii) = readVoltage(a,'A1');
-    %% joyBtn(ii) = readDigitalPin(a,'D2')*4.975;
+    joyBtn(ii) = readDigitalPin(a,'D2')*4.975;
+    btn(ii) = readDigitalPin(a,'D3');
     t(ii) = datetime('now') - t0;
 end 
 
+figure(1);
 plot(t,joy);
-%% scatter(t,joyBtn);
+figure(2);
+plot(t,joyBtn);
+figure(3);
+plot(t,btn);
 %
-% Joystick neutral is around 4.9805 - 4.9775
+% Joystick neutral is around 2.5
