5f316dddb008e95b5a41891b78dd977edcb97f0d
solisabrahan90 <solisabrahan90@gmail.com>
2022-11-09 12:05:48 -0800

    Update Varied_PRS_MODE_V1.ino

--- Haptic/Varied Pressure Code/Varied_PRS_MODE_V1/Varied_PRS_MODE_V1.ino
+++ Haptic/Varied Pressure Code/Varied_PRS_MODE_V1/Varied_PRS_MODE_V1.ino
@@ -2,18 +2,20 @@ int pressureAnalogPin[] = { A0, A1, A2, A3 };  //pin where our pressure pad is l
 int pressureReading;                           //variable for storing our reading
 int motorPins[4] = { 9, 10, 11, 6 };           //connected on pins 9, 10, 11, and 6
 
-int btn = 2; // Puga
-int counter = 0; // Puga
-int btnState = 0; // Puga
+int btn = 2;       // 
+int counter = 0;   // 
+int btnState = 0;  // 
 
 //Adjust these if required.
 int noPressure = 100;         //max value for no pressure on the pad
 int highestPressure = 900;    //max value for medium pressure on the pad
-int maxMotorIntensity = 900;  //max value for highest pressure on the pad
+const int maxMotorIntensity = 900;  //max value for highest pressure on the pad
+
+int motorIntensity;
 
 void setup() {
-  Serial.begin(115200);  // We'll send debugging information via the Serial monitor
-  pinMode(btn, INPUT_PULLUP); // Puga
+  Serial.begin(115200);             // We'll send debugging information via the Serial monitor
+  pinMode(btn, INPUT_PULLUP);       // 
   for (int i = 0; i < 4; i++) {     //loop for pins 9,10,11, and 6
     pinMode(motorPins[i], OUTPUT);  //connected to pin 9
   }
@@ -25,31 +27,26 @@ void loop() {
   }
   // Puga Start
   btnState = digitalRead(btn);
-if ( btnState == LOW) {
-  if (counter < 3){
+  if (btnState == LOW) {
     counter++;
+    if (counter > 3) counter = 0;
+
     Serial.println(counter);
     delay(500);
+
+    if (counter == 1) {
+      Serial.println("Low Vibration");
+      motorIntensity = 0.33 * maxMotorIntensity;
+    }
+    if (counter == 2) {
+      Serial.println("Medium Vibration");
+      motorIntensity = 0.66 * maxMotorIntensity;
+    }
+    if (counter == 3) {
+      Serial.println("High Vibration");
+      motorIntensity = 900;
+    }
   }
-  else {
-    counter = 0;
-  }
-  if (counter == 1){
-  Serial.println("Low Vibration");
-  maxMotorIntensity = 900;
-  maxMotorIntensity = 0.33*maxMotorIntensity;
-}
-if (counter == 2){
-  Serial.println("Medium Vibration");
-  maxMotorIntensity = 900;
-  maxMotorIntensity = 0.66*maxMotorIntensity;
-}
-if (counter == 3){
-  Serial.println("High Vibration");
-   maxMotorIntensity = 900;
-}
-// Puga End
-}
 }
 
 void processMotor(int i) {
@@ -57,7 +54,7 @@ void processMotor(int i) {
   Serial.println(pressureReading);
   if (pressureReading < noPressure) pressureReading = 0;
 
-  int pressureValMapped = map(pressureReading, 0, highestPressure, 0, maxMotorIntensity);
+  int pressureValMapped = map(pressureReading, 0, highestPressure, 0, motorIntensity);
   analogWrite(motorPins[i], pressureValMapped);
 }
 
