0742271a6d956130aaa824a1718cbfd6518fc732
Pogiwogi <116036952+Pogiwogi@users.noreply.github.com>
2022-11-09 16:31:38 -0800

    Add live matlab file, figuring out to work joy in matlab

--- Foot Control/Prototype/Matlab/JoyBtnTest_Data_Aqc.m
+++ Foot Control/Prototype/Matlab/JoyBtnTest_Data_Aqc.m
@@ -9,19 +9,22 @@ btn = zeros(200,1);
 t = seconds(joy);
 configurePin(a,'D2','DigitalInput')
 configurePin(a,'D4','Pullup')
-
+an = animatedline; % Initialize animated line
 t0 = datetime('now');
-for ii = 1:200
-    joy(ii) = readVoltage(a,'A1');
-    joyBtn(ii) = readDigitalPin(a,'D2');
-    btn(ii) = readDigitalPin(a,'D4');
-    t(ii) = datetime('now') - t0;
-end 
-
-%figure(1);
-plot(t,joy);
-figure(2);
-plot(t,joyBtn);
+for i = 1:200
+    t(i) = datetime('now') - t0; % x-value
+    joy(i) = readVoltage(a,'A1'); % y-value
+    plot(t(1:i),joy(i,i));
+    addpoints(an,t(i),joy(i));
+    drawnnow limitrate;
+    %(ii) = readDigitalPin(a,'D2');
+    %btn(ii) = readDigitalPin(a,'D4');
+end
+% https://www.mathworks.com/matlabcentral/answers/83192-plot-in-real-time
+% figure(1);
+% plot(t(1:i,,joy);
+% figure(2);
+% plot(t,joyBtn);
 %figure(3);
 %plot(t,btn);
 
